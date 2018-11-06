f9f197afe6757973309293428b998132a9d51495
LoliMay <lolimay@lolimay.cn>
2018-11-06 10:15:03 +0800

    test ipad pro working copy

--- src/selection-sort.cpp
+++ src/selection-sort.cpp
@@ -10,6 +10,6 @@ int main(int argc, char const *argv[])
 
     for(int i=0; i<5; i++) scanf("%d", &a[i]);
 
-    for(int i=0; i<5; i++) printf("%d ", a);
+    for(int i=0; i<5; i++) printf("%d ", b);
     return 0;
 }
